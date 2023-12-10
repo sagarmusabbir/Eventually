cdc04f76f20d04595a241410ce052bf8688a4677
sagarmusabbir <sagarmusabbir@gmail.com>
2023-12-10 21:53:56 +0600

    theme-setup-for-hugo
    changed image to absolute path and etc

--- static/js/main.js
+++ static/js/main.js
@@ -38,9 +38,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'https://raw.githubusercontent.com/sagarmusabbir/Eventually/master/static/images/bg01.jpg': 'center',
+							'https://raw.githubusercontent.com/sagarmusabbir/Eventually/master/static/images/bg02.jpg': 'center',
+							'https://raw.githubusercontent.com/sagarmusabbir/Eventually/master/static/images/bg03.jpg': 'center'
 						},
 
 					// Delay.
